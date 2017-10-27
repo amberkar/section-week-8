449293f51eba070e3cf86a268af12b7f8e1261ad
amberkar <amberkar@umich.edu>
2017-10-27 09:00:12 -0400

    changed function search_flickr_by_tags

--- flickr.py
+++ flickr.py
@@ -25,7 +25,7 @@ def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
             res.append("{}-{}".format(k, params_d[k]))
     return baseurl + "_".join(res)
 
-def search_flickr_by_tags(tags):
+def search_flickr(tags):
     if not FLICKR_API_KEY:
         raise Exception('Flickr API Key is missing!')
 
@@ -65,7 +65,7 @@ CACHE_DICTION = load_cache_json()
 if DEBUG:
     print(CACHE_DICTION)
 
-results = search_flickr_by_tags('sunset summer')
+results = search_flickr('sunset summer')
 
 photos_list = []
 for r in results['photos']['photo']:
